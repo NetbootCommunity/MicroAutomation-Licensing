fb5d0ddec21c74ad532df7129f8086562e28b3d7
Thomas ILLIET <contact@thomas-illiet.fr>
2022-03-15 00:20:33 +0100

    Add database migration feature

--- src/MicroAutomation.Licensing.Api.Service/Program.cs
+++ src/MicroAutomation.Licensing.Api.Service/Program.cs
@@ -1,27 +1,90 @@
+#region Using
+
+using MicroAutomation.Licensing.Api.Service.Configurations;
+using MicroAutomation.Licensing.Data.Configuration.Configuration;
+using MicroAutomation.Licensing.Data.Shared.DbContexts;
+using MicroAutomation.Licensing.Data.Shared.Helpers;
 using MicroAutomation.Log.Extensions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
+
+#endregion Using
 
 namespace MicroAutomation.Licensing.Api.Service;
 
 public class Program
 {
+    private const string MigrateOnlyArgs = "/migrateonly";
+
     /// <summary>
-    ///
+    /// Main entry point to run this application.
     /// </summary>
     /// <param name="args"></param>
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        CreateHostBuilder(args)
-            .Build().Run();
+        var configuration = GetConfiguration(args).Build();
+
+        Serilog.Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+        try
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            var migrationComplete = await ApplyDbMigrationsAsync(configuration, host);
+            if (args.Any(x => x == MigrateOnlyArgs))
+            {
+                await host.StopAsync();
+                if (!migrationComplete)
+                {
+                    Environment.ExitCode = -1;
+                }
+
+                return;
+            }
+
+            await host.RunAsync();
+        }
+        catch (Exception ex)
+        {
+            Serilog.Log.Fatal(ex, "Host terminated unexpectedly");
+        }
+        finally
+        {
+            Serilog.Log.CloseAndFlush();
+        }
     }
 
     /// <summary>
-    ///
+    /// Apply database migration.
+    /// </summary>
+    /// <param name="configuration"></param>
+    /// <param name="host"></param>
+    /// <returns></returns>
+    private static async Task<bool> ApplyDbMigrationsAsync(IConfiguration configuration, IHost host)
+    {
+        var testingConfiguration = configuration.GetSection(nameof(TestingConfiguration)).Get<TestingConfiguration>();
+        if (testingConfiguration?.IsStaging == false)
+        {
+            var databaseMigrationsConfiguration = configuration.GetSection(nameof(DatabaseMigrationsConfiguration))
+                .Get<DatabaseMigrationsConfiguration>();
+
+            return await DbMigrationHelpers
+                .ApplyDbMigrationsAsync<DataStoreDbContext, DataProtectionDbContext>(host, databaseMigrationsConfiguration);
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// Create host builder.
     /// </summary>
     /// <param name="args"></param>
     /// <returns></returns>
@@ -39,29 +102,42 @@ public class Program
         builder.UseCustomSerilog();
 
         // Sets up the configuration for the remainder of the build process and application
-        builder.ConfigureAppConfiguration((hostContext, config) =>
-        {
-            // Retrieve the name of the environment
-            var aspnetcore = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var dotnetcore = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
-            var environmentName = string.IsNullOrWhiteSpace(aspnetcore) ? dotnetcore : aspnetcore;
-            if (string.IsNullOrWhiteSpace(environmentName))
-                environmentName = "Production";
-
-            // Define the configuration builder
-            config.SetBasePath(pathContextRoot);
-            config.AddJsonFile("appsettings.json", optional: false);
-            config.AddJsonFile($"appsettings.{environmentName}.json", optional: true);
-            config.AddEnvironmentVariables();
-            config.AddCommandLine(args);
-        });
+        builder.ConfigureAppConfiguration((_, config) => config = GetConfiguration(args));
 
         // Configures a HostBuilder with defaults for hosting a web app.
-        builder.ConfigureWebHostDefaults(webBuilder =>
-        {
-            webBuilder.UseStartup<Startup>();
-        });
+        builder.ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
 
         return builder;
+    }
+
+    /// <summary>
+    /// Build and return product configuration.
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    private static IConfigurationBuilder GetConfiguration(string[] args)
+    {
+        // Retrieve the name of the environment
+        var aspnetcore = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        var dotnetcore = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+        var environment = string.IsNullOrWhiteSpace(aspnetcore) ? dotnetcore : aspnetcore;
+        if (string.IsNullOrWhiteSpace(environment))
+            environment = "Production";
+        var isDevelopment = environment == Environments.Development;
+
+        var configurationBuilder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+            .AddJsonFile("serilog.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"serilog.{environment}.json", optional: true, reloadOnChange: true);
+
+        if (isDevelopment)
+            configurationBuilder.AddUserSecrets<Startup>(true);
+
+        configurationBuilder.AddCommandLine(args);
+        configurationBuilder.AddEnvironmentVariables();
+
+        return configurationBuilder;
     }
 }
